a4f520da987e4f96f21a95af1182e75b4646f7df
KimYoHwan <radline000@gmail.com>
2021-01-05 13:56:17 +0900

    commit

--- youtube_clone/app.js
+++ youtube_clone/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import morgan  from "morgan";
+import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
@@ -7,21 +7,21 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
-import {localsMiddleware} from "./middlewares";
+import { localsMiddleware } from "./middlewares";
 const app = express();
 
-app.set("view engine","pug");
-app.use("/uploads",express.static("uploads"))
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 
-app.use(routes.home,globalRouter);
-app.use(routes.users,userRouter);
-app.use(routes.videos,videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
